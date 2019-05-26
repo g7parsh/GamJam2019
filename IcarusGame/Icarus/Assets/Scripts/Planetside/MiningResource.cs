a22bcedef62391dad5c23a39e93e247075afb945
Parshva <g7parsh@gmail.com>
2019-05-26 00:43:52 -0400

    Changed structure of resource inv from list to dict
    also the mats have different colors now

--- IcarusGame/Icarus/Assets/Scripts/Planetside/MiningResource.cs
+++ IcarusGame/Icarus/Assets/Scripts/Planetside/MiningResource.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class MiningResource : MonoBehaviour
 {
     public int Quantity = 50;
-    public Inventory.RESOURCE type;
+    public Inventory.RESOURCE resourceType;
+    public MaterialPropertyBlock block;
+    private Renderer renderer;
 
+    void Awake()
+    {
+    }
     // Start is called before the first frame update
     void Start()
+
     {
-        
+        block = new MaterialPropertyBlock();
+        renderer = GetComponentInChildren<Renderer>();
+        renderer.GetPropertyBlock(block);
+        switch (resourceType)
+        {
+
+            case Inventory.RESOURCE.FUEL:
+                block.SetColor("_Color", Color.red);
+                renderer.SetPropertyBlock(block);
+                break;
+            case Inventory.RESOURCE.HEALTH:
+                block.SetColor("_Color", Color.green);
+                renderer.SetPropertyBlock(block);
+                break;
+            case Inventory.RESOURCE.REPAIR:
+                block.SetColor("_Color", Color.grey);
+                renderer.SetPropertyBlock(block);
+                break;
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerInventory>()!= null) {
+            PlayerInventory inventory = other.gameObject.GetComponent<PlayerInventory>();
+            inventory.AddResource(this, Quantity);
+        }
     }
 }
