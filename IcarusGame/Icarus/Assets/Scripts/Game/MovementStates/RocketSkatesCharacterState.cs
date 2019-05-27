976ef8da34ac155db982b3b8f559e854d02d5b66
Dom <domeniccristaldi@gmail.com>
2019-05-26 18:45:23 -0700

    SKAAAATES!!!

---
+++ IcarusGame/Icarus/Assets/Scripts/Game/MovementStates/RocketSkatesCharacterState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterRocketSkatesState", menuName = "Icarus/Character States/Rocket Skates")]
+public class RocketSkatesCharacterState : BaseCharacterState
+{
+    public override EStateContext CalculateMovement(MovementContext movementContext)
+    {
+
+
+        return EStateContext.Running;
+    }
+}
